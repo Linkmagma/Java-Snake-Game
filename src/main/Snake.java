e0adafa1aacac1e2efd383299e06786fc2d7f09e
Cody Pratt <codykpratt@gmail.com>
2025-01-07 15:34:11 -0700

    Added Methods for moving, growing, and collisions

--- src/main/Snake.java
+++ src/main/Snake.java
@@ -3,6 +3,18 @@ package main;
 public class Snake {
 	
 	
+	public void Move() {
+		
+	}
+	
+	public void Grow() {
+		
+	}
+	
+	public void Collision() {
+		
+	}
+	
 	public enum Direction {
 		UP, DOWN, LEFT, RIGHT;
 	}
