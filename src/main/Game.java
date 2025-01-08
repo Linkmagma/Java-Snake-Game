891631ab853d58352aced3de3fa3b19d5e9fcb4b
Cody Pratt <codykpratt@gmail.com>
2025-01-08 12:05:17 -0700

    Create main game components

--- src/main/Game.java
+++ src/main/Game.java
@@ -8,10 +8,22 @@ game loop, game state (Game Over)
 package main;
 
 import java.awt.*;
-import java.awt.event.KeyEvent; // Captures key presses (Arrow Keys)
-import java.util.List;
+import java.awt.event.*; // Captures key presses (Arrow Keys)
+import javax.swing.*;
 
 
-public class Game {
+public class Game implements ActionListener {
 	
+	private Snake snake; // Snake Object
+	private Food food; // Food Object
+	private boolean active; // Game: Active (true) Terminated (false)
+	private Timer timer; // Timer starts game loop
+	private int score; // Player score
+	
+	public Game() {
+		this.snake = new Snake();
+		this.food = new Food(); // Initialize snake and food
+		this.active = true;
+		this.score = 0; // Set score to 0 and game to true
+		this.timer = new Timer(100, this);
 }
