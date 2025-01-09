f0152f30b0aac5b1c54f8e3dc15657a342205531
Cody Pratt <codykpratt@gmail.com>
2025-01-08 22:38:02 -0700

    Set up KeyListener and created JFrame and GamePanel

--- src/main/Main.java
+++ src/main/Main.java
@@ -14,6 +14,34 @@ import java.awt.event.KeyEvent;
 public class Main {
 	
     public static void main(String[] args) {
+    	JFrame frame = new JFrame("Snake Game"); // Window title
+    	GamePanel panel = new GamePanel(); // Game panel for graphics inside JFrame
+    	Game game = new Game(); // Game object
+    	
+    	frame.setSize(500, 500);; // Window size
+    	frame.add(panel); // Add panel to frame
+    	
+    	// KeyListener for key presses
+    	
+    	frame.addKeyListener(new KeyAdapter() {
+    		@Override
+    		public void keyPressed(KeyEvent e) {
+    			// Detect key pressed
+    			int keyCode = e.getKeyCode();
+    			if (keyCode == KeyEvent.VK_UP) {
+    				// game.getSnake().setDirection(0);
+    			} else if (keyCode == KeyEvent.VK_RIGHT) {
+    				
+    			} else if (keyCode == KeyEvent.VK_DOWN) {
+    				
+    			} else if (keyCode == KeyEvent.VK_LEFT) {
+    				
+    			}
+    		}
+    	});
+    	
+    	game.start();
+    	frame.setVisible(true); // Window = visible
     	
     }
 }
--- src/main/Snake.java
+++ src/main/Snake.java
@@ -15,15 +15,7 @@ public class Snake {
 	private LinkedList<Point> body; // Snake's Body using LinkedList
 	private int direction; // Snake direction (Passes to Game.java where it is updated on game board)
 	private final int SIZE = 10; // Each snake segment = amount of pixels specified
-	
-	public void Move() {
-		
-	}
-	
-	public void Grow() {
-		
-	}
-	
+
 	public Snake() {
 		body = new LinkedList<>();
 		body.add(new Point(50, 50)); // Snake head start position
@@ -35,18 +27,50 @@ public class Snake {
 	
 	// Move snake in direction by adding head segment and removing last body segment
 	public void move() {
-		Point head = body.getHead(); // Get current snake head
+		Point head = body.getFirst(); // Get current snake head
 		Point newHead = null;
 		
 		// Use switch to change direction that head is added
 		switch (direction) {
-		case 0;
-		case 1;
-		case 2;
-		case 3;
+			case 0:
+				newHead = new Point(head.x, head.y - SIZE); // Decrease y to move upwards
+				break;
+			case 1:
+				newHead = new Point(head.x + SIZE, head.y); // Increase x to move right
+				break;
+			case 2:
+				newHead = new Point(head.x, head.y + SIZE); // Increase y to move down
+				break;
+			case 3:
+				newHead = new Point(head.x - SIZE, head.y); // Decrease x to move left
+				break;
 		}
 		
-		body.addHead(newHead); // Add new head at the front of the LinkedList
-		body.removeHead(); // Remove last list item or tail of snake
+		body.addFirst(newHead); // Add new head at the front of the LinkedList
+		body.removeLast(); // Remove last list item or tail of snake
 	}
+	
+	public void grow() {
+		Point tail = body.getLast(); // Get the last segment of the snake (the tail)
+		body.addLast(tail); // Add segment at the end of the snake to keep it longer
+	}
+	
+	// Getter methods for accessing snake's body and direction
+	
+	public LinkedList<Point> getBody() {
+		return body;
+	}
+	
+	public int getDirection() {
+		return direction;
+	}
+	
+	public Point getHead() {
+		return body.getFirst(); // Return the head of the snake
+	}
+	
+	public void setDirection(int newDirection) {
+		direction = newDirection;
+	}
+	
 }
