2e74299aa1757ab99936a3727e4812320c0b3849
xorn73 <xorn73@gmail.com>
2025-01-13 13:57:56 -0700

    food location logic

--- src/main/Food.java
+++ src/main/Food.java
@@ -4,9 +4,21 @@ Food item that is randomly positioned on game grid
 
 */
 import  java.util.Random;
+import java.awt.*;
 
 package main;
 
 public class Food {
-
+	private Random gen = new Random();
+	private int x;
+	private int y;
+	private Point location;
+	public Food() {
+		
+	}
+	public void newLocation() {
+		x = gen.nextInt(0,49) * 10;
+		y = gen.nextInt(0,49) * 10;
+		location = new Point(x,y);
+	}
 }
