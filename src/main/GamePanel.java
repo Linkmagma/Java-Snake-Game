821e2aa007b26d4f60d9cd4745818e6d4ff64ba3
xorn73 <xorn73@gmail.com>
2025-01-14 08:21:06 -0700

    idk

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -14,13 +14,14 @@ import java.util.List;
 public class GamePanel extends JPanel {
 	
 	private Game game;
+	public final Dimension SCALE = new Dimension(10, 10);
 	
 	public GamePanel() {
 		this.game = new Game(); // Initialize Game
 		setPreferredSize(new Dimension(500, 500)); // Size of GamePanel
 		setBackground(Color.BLACK);
 	}
-	
+
 	public void gameActive() {
 		
 	}
