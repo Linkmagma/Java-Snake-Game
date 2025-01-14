2527e98c143287660345b60afb9e3f46d152167f
Cody Pratt <codykpratt@gmail.com>
2025-01-14 12:29:52 -0700

    Updated snake class

--- src/main/Snake.java
+++ src/main/Snake.java
@@ -15,7 +15,7 @@ public class Snake {
 	private LinkedList<Point> body; // Snake's Body using LinkedList
 	private int direction; // Snake direction
 	private final int SIZE = 10; // Each snake segment = amount of pixels specified
-
+	
 	public Snake() {
 		body = new LinkedList<>();
 		body.add(new Point(50, 50)); // Snake head start position
@@ -26,28 +26,32 @@ public class Snake {
 	}	 
 	
 	// Move snake in direction by adding head segment and removing last body segment
+	
 	public void move() {
-		Point head = body.getFirst(); // Get current snake head
-		Point newHead = null;
 		
-		// Use switch to change direction that head is added
-		switch (direction) {
-			case 0:
-				newHead = new Point(head.x, head.y - SIZE); // Decrease y to move upwards
-				break;
-			case 1:
-				newHead = new Point(head.x + SIZE, head.y); // Increase x to move right
-				break;
-			case 2:
-				newHead = new Point(head.x, head.y + SIZE); // Increase y to move down
-				break;
-			case 3:
-				newHead = new Point(head.x - SIZE, head.y); // Decrease x to move left
-				break;
-		}
+			Point head = body.getFirst(); // Get current snake head
+			Point newHead = null;
+			
+			// Use switch to change direction that head is added
+			
+			switch (direction) {
+				case 0:
+					newHead = new Point(head.x, head.y - SIZE); // Decrease y to move upwards
+					break;
+				case 1:
+					newHead = new Point(head.x + SIZE, head.y); // Increase x to move right
+					break;
+				case 2:
+					newHead = new Point(head.x, head.y + SIZE); // Increase y to move down
+					break;
+				case 3:
+					newHead = new Point(head.x - SIZE, head.y); // Decrease x to move left
+					break;
+			}
 		
 		body.addFirst(newHead); // Add new head at the front of the LinkedList
 		body.removeLast(); // Remove last list item or tail of snake
+		
 	}
 	
 	public void grow() {
